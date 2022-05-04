1a183787379eeaedb7f84e8036f3b758753fa03d
forkys <forlettadavide@gmail.com>
2022-05-04 10:20:49 +0200

    PARAMS for intervals time

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ setInterval(function() {
         console.log("update values amount");
         portfolio_manage.add_amount_transcations(client_coinbase);
     }
-}, 10000);
+}, parseFloat(vars.INTERVAL_SECONDS_UPDATE_PORTFOLIO) * 1000);
 
 if(vars.BOOL_WRITE_CSV){
     setInterval(function() {
         csv_management.write_csv(portfolio_manage.get_portfolio());
-    }, 30000 );
+    }, parseFloat(vars.INTERVAL_SECONDS_WRITE_CSV) * 1000 );
 }
 
 
@@ -32,5 +32,5 @@ if(vars.BOOL_WRITE_CSV){
 if(vars.BOOL_REPORT_CSV){
     setInterval(function() {
         mail_management.send_mail_report();
-    }, 60000  * 60 * 3);
+    }, 60000  * parseFloat(vars.INTERVAL_MINUTES_SEND_CSV_BY_MAIL));
 }
